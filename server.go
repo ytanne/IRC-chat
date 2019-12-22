c1c9c9faac737fb7fb71eb7ba624e77217ec6b9d
Yerzhan Orazayev <yerzhan1993@gmail.com>
2019-12-22 00:14:36 -0800

    update

--- server.go
+++ server.go
@@ -4,13 +4,29 @@ import (
     "fmt"
 	"net"
 	"os"
-	//"strings"
+	"encoding/json"
+	//"bufio"
+	//"io"
+
+	"github.com/gorilla/websocket"
+	"strings"
 )
 
-type user struct {
-	password string
-	username string
-	nickname string
+type User struct {
+	Password 	string
+	Username 	string
+	Nickname 	string
+}
+
+type Message struct {
+	Text string `json:"text"`
+}
+
+type hub struct {
+	clients          map[string]*websocket.Conn
+	addClientChan    chan *websocket.Conn
+	removeClientChan chan *websocket.Conn
+	broadcastChan    chan Message
 }
 
 func	main() {
@@ -26,14 +42,7 @@ func	main() {
 }
 
 func	handleConnection(conn net.Conn) {
-	var	credentials[3] string
-
-	conn.Write([]byte("Enter username: "))
-	credentials[0] = getData(conn)
-	conn.Write([]byte("Enter password: "))
-	credentials[1] = getData(conn)
-	conn.Write([]byte("Enter nickname: "))
-	credentials[2] = getData(conn)
+	getUserCredentials(conn)
 	//fmt.Println(conn.RemoteAddr().String())
 	conn.Close()
 }
@@ -54,3 +63,98 @@ func	getData(conn net.Conn) string {
 	//conn.Write([]byte("Message received.\n"))
 	return (string(buf[:reqLen]))
 }
+
+func	getUserCredentials(conn net.Conn) {
+	var	person User
+
+	var command = getData(conn)
+	person.Password = checkCommand(command, "PASS")
+	person.Password = person.Password[:len(person.Password) - 1]
+	if (person.Password == "") {
+		go closeConn(conn, person)
+	}
+	command = getData(conn)
+	person.Nickname = checkCommand(command, "NICK")
+	person.Nickname = person.Nickname[:len(person.Nickname) - 1]
+	if (person.Password == "") {
+		go closeConn(conn, person)
+	}
+	command = getData(conn)
+	person.Username = checkCommand(command, "USER")
+	person.Username = person.Username[:len(person.Username) - 1]
+	if (person.Password == "") {
+		go closeConn(conn, person)
+	}
+	if (person != User{}) {
+		b, err := json.Marshal(person)
+		errorHandling(err)
+		saveCredentials(b)
+	}
+	/*
+	conn.Write([]byte("Enter username: "))
+	person.Username = getData(conn)
+	conn.Write([]byte("Enter password: "))
+	person.Password = getData(conn)
+	conn.Write([]byte("Enter nickname: "))
+	person.Nickname = getData(conn)
+	checkCredentials(person)
+	b, err := json.Marshal(person)
+	errorHandling(err)
+	saveCredentials(b)
+	*/
+}
+
+func	closeConn(conn net.Conn, person User) {
+	conn.Write([]byte("Wrong input\n"))
+	person.Password = ""
+	person.Nickname = ""
+	person.Username = ""
+}
+
+func	checkCommand(input string, goal string) string {
+	var array = strings.Split(input, " ")
+	
+	if array != nil && len(array) == 2 && array[0] == goal {
+		return array[1]
+	} else {
+		return ""
+	}
+}
+
+
+func	saveCredentials(credentials []byte) {
+	f, err := os.OpenFile("users.log",
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		errorHandling(err)
+	}
+	defer f.Close()
+	if _, err := f.WriteString(string(credentials) + "\r\n"); err != nil {
+		errorHandling(err)
+	}
+}
+/*
+func	checkCredentials(person User) bool {
+
+	file, err := os.Open("users.log")
+	if err != nil {
+		errorHandling(err)
+	}
+
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, _, err := reader.ReadLine()
+
+		if err == io.EOF {
+			return (false)
+		}
+		if err := json.Unmarshal(line, &person); err != nil {
+			errorHandling(err)
+		}
+		fmt.Printf("%s \n", line)
+	}
+}
+*/
